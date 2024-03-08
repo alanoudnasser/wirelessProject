ee721f22e0fdffd6987ce617b1c5a56d759ffaa6
AshwaqAlsubaie <ashwaqalsubaie3@gmail.com>
2024-03-08 17:27:02 +0300

    add code

--- app/src/main/java/com/example/wirelessproject/MainActivity.java
+++ app/src/main/java/com/example/wirelessproject/MainActivity.java
@@ -4,11 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText newItemEditText;
+    private Button addButton;
+    private ListView itemList;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newItemEditText = findViewById(R.id.newItemEditText);
+        addButton = findViewById(R.id.addButton);
+        itemList = findViewById(R.id.itemList);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        itemList.setAdapter(adapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem = newItemEditText.getText().toString();
+                if (!newItem.isEmpty()) {
+                    items.add(newItem);
+                    adapter.notifyDataSetChanged();
+                    newItemEditText.setText("");
+                }
+            }
+        });
+
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        itemList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
